a41d8893ac956eada045f3b05996739696861bd8
DESKTOP-QBB5DTQ\sabbi <nsabbir26@gmail.com>
2024-10-26 23:14:34 +0600

    - Initial Commit

---
+++ print_comment.py
@@ -0,0 +1,8 @@
+#This is the day 1 of learning Python!
+
+# print statement
+print("Yo This is the first python code that I wrote!")
+print("It's really good tbh!")
+
+# comment
+##### <----- use this to comment
