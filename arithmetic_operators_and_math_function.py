d3c5dbdbdf5ddfd54348f50cee3a70fa9726315c
DESKTOP-QBB5DTQ\sabbi <nsabbir26@gmail.com>
2024-11-03 23:49:20 +0600

    - some arithmetic operators - augmented assignment operators

---
+++ arithmetic_operators_and_math_function.py
@@ -0,0 +1,26 @@
+#Operators and Math function
+from pyexpat import features
+
+friends = 10
+
+#short form - augmented assignment operator
+#friends = friends + 1
+#friends += 1
+
+#friends = friends - 2
+#friends -= 2
+
+#friends = friends * 3
+#friends *= 3
+
+#friends = friends / 2
+# friends /= 2
+
+# friends = friends ** 2
+# friends **= 2
+
+reminder = friends % 4
+
+
+print(f"Now I have {friends} friends.")
+print(f"Reminder:{reminder}")
