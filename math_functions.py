4017f5c5960853249c2bb181548d4f24a740d2b8
DESKTOP-QBB5DTQ\sabbi <nsabbir26@gmail.com>
2024-11-04 22:50:44 +0600

    - built in math functions

---
+++ math_functions.py
@@ -0,0 +1,25 @@
+# Built-in Math functions
+
+x = 3.14
+y = -4
+z = 5
+
+# round function
+roundResult = round(x)
+print(f"Rounded value {roundResult}")
+
+# abs function - distance from 0 as whole number
+absResult = abs(y)
+print(f"Abs value {absResult}")
+
+# power function
+powerResult = pow(4, 3)
+print(f"Power value {powerResult}")
+
+# max function - to find max value between values
+maxResult = max(x,y,z)
+print(f"Max value {maxResult}")
+
+# min function - to find min value between values
+minResult = min(x,y,z)
+print(f"Min value {minResult}")
