2de45edcbd264b4355b70eb6085dd8f21686b582
DESKTOP-QBB5DTQ\sabbi <nsabbir26@gmail.com>
2024-12-04 01:53:54 +0600

    - updated floor function

---
+++ math_class_cons&func.py
@@ -0,0 +1,15 @@
+import math
+
+x = 9
+y = 99.1
+print(math.pi)
+print(math.e)
+
+squareRootResult = math.sqrt(x)
+print(f"Square root of {x} is : {squareRootResult}")
+
+ceilResult = math.ceil(y)
+print(f"Ceil result of {y} is : {ceilResult}")
+
+floorResult = math.floor(y)
+print(f"Floor result of {y} is : {floorResult}")
